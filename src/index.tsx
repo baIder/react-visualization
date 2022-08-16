b95bc7e6103c93f6851d0802316dd5f0b7baa5f1
baIder <511763880@qq.com>
2022-08-16 15:56:17 +0800

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {HashRouter as Router} from 'react-router-dom';
+import './index.scss';
 import App from './App';
 
 const root = ReactDOM.createRoot(
@@ -8,7 +9,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App/>
+    </Router>
   </React.StrictMode>
 );
 
