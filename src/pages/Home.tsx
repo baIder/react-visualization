7a31c08582dc86ce7069e6a5920de8d1ce8e4bda
baIder <511763880@qq.com>
2022-08-16 16:40:18 +0800

    使用grid布局划分区域

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,13 @@ const Home: React.FC = () => {
     <>
       <div className="home">
         <header style={{backgroundImage: `url(${headerBg})`}}/>
+        <main>
+          <section className="section1"></section>
+          <section className="section2"></section>
+          <section className="section3"></section>
+          <section className="section4"></section>
+          <section className="section5"></section>
+        </main>
       </div>
     </>
   );
